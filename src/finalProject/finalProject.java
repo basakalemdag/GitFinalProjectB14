5dffb6ce1bd011ab462b190e1702444be67506d4
basakalemdag <basakalemdag@gmail.com>
2022-09-29 03:48:19 +0300

    second commit

--- src/finalProject/finalProject.java
+++ src/finalProject/finalProject.java
@@ -3,7 +3,8 @@ package finalProject;
 public class finalProject {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("this is a code for the Mercedes");
 
 	}
 
